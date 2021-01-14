1284ff1e1eae2e27e7ce821f136efdf10195ff6c
Brandon Hudson <bhud07@gmail.com>
2021-01-14 13:26:26 -0500

    Fixes SPM Package.swift location to be in root of project

--- Package.swift
+++ Package.swift
@@ -27,7 +27,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Cardian",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire"],
+            path: "Cardian/",
+            sources: ["Source"]
+        ),
         .testTarget(
             name: "CardianTests",
             dependencies: ["Cardian"]),
