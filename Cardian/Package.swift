691743d222325302b97ecd3ee7b969a3b0b3fd14
Brandon Hudson <bhud07@gmail.com>
2021-01-14 13:35:02 -0500

    Move Package.swift back

--- Cardian/Package.swift
+++ Cardian/Package.swift
@@ -27,9 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Cardian",
-            dependencies: ["Alamofire"],
-            path: "Cardian/",
-            sources: ["Source"]
+            dependencies: ["Alamofire"]
         ),
         .testTarget(
             name: "CardianTests",
