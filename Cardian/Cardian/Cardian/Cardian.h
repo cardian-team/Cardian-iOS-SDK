0723063c6acb3803e799405fd46785cf8e55f9a1
Brandon Hudson <bhud07@gmail.com>
2021-01-14 12:05:22 -0500

    Working commit to add initial project for Carthage

---
+++ Cardian/Cardian/Cardian/Cardian.h
@@ -0,0 +1,18 @@
+//
+//  Cardian.h
+//  Cardian
+//
+//  Created by Brandon Hudson on 1/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cardian.
+FOUNDATION_EXPORT double CardianVersionNumber;
+
+//! Project version string for Cardian.
+FOUNDATION_EXPORT const unsigned char CardianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cardian/PublicHeader.h>
+
+
